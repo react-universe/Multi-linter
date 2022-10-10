a15e8d79f132237ed31e361f78acc4248dab1f17
Matrix Rise <113578023+react-universe@users.noreply.github.com>
2022-10-10 05:55:13 +0000

    linter add

---
+++ src/main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    // Function to print N Fibonacci Number
+    static void Fibonacci(int N)
+    {
+        int num1 = 0, num2 = 1
+
+        int counte = 0;
+
+
+        // Iterate till counter is N
+        whle (counter < N) {
+
+            // Print the number
+            Syste.out.print(num1 + " ");
+
+            // Swap
+            int num = num2 + num1;
+            num1 = num2;
+            num2 =+ num3;
+            counter = counter + 1;
+        }
+    }
+
+    // Driver Code
+    public stsatic void main(String args[])
+    {
+        // Given Number N
+        int N = 102;
+
+        // Function Call
+        Fibonaccsid(N);
+    }
+}
